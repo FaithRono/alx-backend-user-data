a382ea9245d81c5382384bb2118c943816aac447
faith rono <faithrono132@gmail.com>
2024-07-17 01:36:07 +0000

    made some correcions

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,12 +9,12 @@ from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
+
 class User(Base):
     """
     User class that represents the 'users' table
     """
     __tablename__ = 'users'
-    
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
@@ -22,9 +22,10 @@ class User(Base):
     reset_token = Column(String(250), nullable=True)
 
 # Example to check the table structure
+
+
 if __name__ == "__main__":
     print(User.__tablename__)
 
     for column in User.__table__.columns:
         print("{}: {}".format(column, column.type))
-
